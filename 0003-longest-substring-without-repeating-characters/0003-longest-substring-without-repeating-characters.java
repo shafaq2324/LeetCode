d7cc19213abf7761badd60565819eee6ba4c3d5b
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-03-05 19:41:01 +0530

    Time: 5 ms (86.43%), Space: 45.4 MB (83.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,7 +7,7 @@ class Solution {
             char c = s.charAt(r); // extract the rightmost char of window
             if(seen.containsKey(c)){
                 // if character repeats start new window from position one ahead of last occurence of left
-                l = seen.get(c) + 1; // one ahead of the seen index
+                l = Math.max(l, seen.get(c) + 1); // one ahead of the seen index
             }
             seen.put(c, r); // if new character add in map
             maxlen = Math.max(maxlen, r - l + 1); // calculate window size
