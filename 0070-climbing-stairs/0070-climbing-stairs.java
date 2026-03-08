0dce76ddad429dba37a2e4f234c9f9e69cb58026
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-03-08 21:55:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 3){
-            return n;
+        if(n == 1 || n == 0){
+            return 1;
         }
-        int prev1 = 3;
-        int prev2 = 2;
-        int curr = 0;
 
-        for(int i = 3; i < n; i++){
-            curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        } 
-        return curr;
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }
