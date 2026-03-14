902ac3618d60d3848c8e9a495ae5fb24e17f34e4
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-03-14 12:52:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n <= 0) return false;
-        while(n % 4 == 0){
-            n /= 4;
+        if(n<=0) return false;
+        while(n% 4==0){
+            n/=4;
         }
-        return n == 1;
+        return n==1;
     }
 }
