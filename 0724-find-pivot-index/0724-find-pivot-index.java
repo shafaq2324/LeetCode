9720e75d70b9c4c26a45b15b635ebb6123662efb
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-03-31 21:45:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -4,9 +4,11 @@ class Solution {
         int sumright[] = new int[nums.length];
         for(int i = 0; i < sumleft.length - 1; i++){
             sumleft[i + 1] = sumleft[i] + nums[i];
+            //current element + previous left sum = next left sum
         }
         for(int i = sumright.length - 1; i > 0; i--){
             sumright[i - 1] = nums[i] + sumright[i];
+            // while filling the array from the end add previous sum along with current element 
         }
         for(int i = 0; i < sumleft.length; i++){
             if(sumleft[i] == sumright[i]){
