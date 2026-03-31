867adc1e67cd5d640564eca48a2c6a7349994226
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-03-31 21:48:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,14 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int sumleft[] = new int[nums.length];
-        int sumright[] = new int[nums.length];
-        for(int i = 0; i < sumleft.length - 1; i++){
-            sumleft[i + 1] = sumleft[i] + nums[i];
-            //current element + previous left sum = next left sum
+        int total = 0; // total sum
+        int left = 0; // left sum
+        for(int i: nums) total += i; // sum of all elements
+
+        for(int i = 0; i < nums.length; i++){
+            if(left == total - left - nums[i]) return i;
+            // right = total - left
+            left += nums[i]; // add current element in left sum
         }
-        for(int i = sumright.length - 1; i > 0; i--){
-            sumright[i - 1] = nums[i] + sumright[i];
-            // while filling the array from the end add previous sum along with current element 
-        }
-        for(int i = 0; i < sumleft.length; i++){
-            if(sumleft[i] == sumright[i]){
-                return i;
-            }
-        }
-        return -1;
+        return -1; 
     }
 }
