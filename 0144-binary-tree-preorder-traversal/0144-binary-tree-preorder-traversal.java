bc42835055685c9a480140ceaf50772184bde7bf
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-21 19:58:02 +0530

    Time: 0 ms (100%), Space: 43.2 MB (35.14%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -15,14 +15,25 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        Pre(root, ans);
-        return ans;
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        // stack keeps track of most recent root
+        List<Integer> pre = new ArrayList<>();
+        // stores the pre order traversal
+        if(root == null) return pre;
+        // if root is null return empty list
+        st.push(root);
+        // push the current root in stack
+        while(!st.isEmpty()){
+            root = st.pop();
+            // pop the most recent root every time
+            pre.add(root.val);
+            // add the root's value to pre
+            if(root.right != null) st.push(root.right);
+            // add the right first into the stack as it follows LIFO, check for null 
+            if(root.left != null) st.push(root.left);
+            // add root's left to stack if it exists
+        }
+        return pre;
     }
-    private static void Pre(TreeNode root, List<Integer> ans){
-        if(root == null) return;
-        ans.add(root.val);
-        Pre(root.left, ans);
-        Pre(root.right, ans);
-    }
+    
 }
