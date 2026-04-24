84bfaab9ba8b290c80974753ad2be087fa51cc23
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-24 12:42:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,22 +15,20 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        int lh = check(root.left);
-        int rh = check(root.right);
-        if(Math.abs(lh - rh) > 1) return false; 
-        // if difference in height exceeds one then it is not balanced
-        boolean left = isBalanced(root.left);
-        boolean right = isBalanced(root.right);
-        // recursive calls for left and right
-        if(!left || !right) return false;
-        // if any one is false return false otherwise true
-        return true;
+        return check(root) != -1;
+        // optimized code that only checks for failed binary trees which would return -1
     }
     private int check(TreeNode root){
         if(root == null) return 0;
+        // if root is null it would contribute 0 to the height, also as it is balanced it would return 0 which is not equal to -1
         int lh = check(root.left);
         int rh = check(root.right);
+        //recursive calls for left and right
+        if(Math.abs(lh - rh) > 1) return -1;
+        // check if their absolute difference is greater than one, if yes then it is a failed case
+        if(lh == -1 || rh == -1) return -1; 
+        // if either one of them becomes -1 no need to check further
         return Math.max(lh, rh) + 1;
+        // else simple return height of the tree
     }
 }
