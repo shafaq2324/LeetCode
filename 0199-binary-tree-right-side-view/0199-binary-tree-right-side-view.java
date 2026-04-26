bfc9d85eb3af33b7e81eac11c8b0ea99d469ff7f
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-26 22:50:27 +0530

    Time: 0 ms (100%), Space: 43.4 MB (92.33%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -3,7 +3,7 @@
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
- *     TreeNode right;
+ *   TreeNode right;
  *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
@@ -21,8 +21,11 @@ class Solution {
     }
     private static void BFS(TreeNode root, int level, List<Integer> ans){
         if(root == null) return;
-        if(ans.size() == level) ans.add(root.val);
+        if(ans.size() == level) ans.add(root.val); //add the node value only if the level size = ans size, as that level is being explored for the first time
         BFS(root.right, level + 1, ans);
+        // explore rightmost elements first
         BFS(root.left, level + 1, ans);
+        // explore left if and only if there is no right node covering it 
     }
+    // for left side view explore the left subtree first
 }
