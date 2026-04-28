f34d35ac83192bc6017ab44fa84505dcafa78e98
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-29 01:48:39 +0530

    Time: 1 ms (100%), Space: 48.9 MB (52.45%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -4,7 +4,9 @@ class Solution {
         int m = nums2.length;
 
         int p1, p2, p3;
+        // Pointers for nums1, nums2, and the merged array respectively
         p1=p2=p3 = 0;
+        // combined array
         int arr[] = new int[m+n];
         while(p1 < n && p2 < m){
             if(nums1[p1] <= nums2[p2]){
@@ -15,14 +17,16 @@ class Solution {
             }
         }
 
-        while (p1 < n) arr[p3++] = nums1[p1++];
-        while (p2 < m) arr[p3++] = nums2[p2++];
+        while (p1 < n) arr[p3++] = nums1[p1++]; // if nums1 has remaining elements
+        while (p2 < m) arr[p3++] = nums2[p2++]; // if nums2 has remaining elements
 
         if((m+n)%2 == 1){
             return (double)arr[(m+n)/2];
+            // If the total number of elements is odd, return the middle element
         }
         else{
             return (double)(arr[(m+n)/2] +arr[((m+n)/2 ) - 1])/2;
+            // If even, return the average of the two middle elements
         }
     }
 }
