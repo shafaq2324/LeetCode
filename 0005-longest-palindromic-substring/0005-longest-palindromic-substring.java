6644ba3084e9f4ff99d6bd21203d31d57eccb211
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-29 01:51:40 +0530

    Time: 491 ms (14.9%), Space: 47.2 MB (14.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -3,13 +3,13 @@ class Solution {
         int n = s.length();
 
         for(int len = n; len > 0; len--) {
-
+        // iterate via all lengths of palindromic strings
             for(int i = 0; i <= n - len; i++) {
-
+                // slide window of len size 
                 String sub = s.substring(i, i + len);
-
+                // extract substring from current window 
                 if(isPalindrome(sub)) {
-                    return sub;
+                    return sub; // check if palindrome
                 }
             }
         }
