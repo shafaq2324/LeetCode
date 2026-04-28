6fafa83da0d6cbe49bbe085b00d173ce006bc119
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-29 02:41:11 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.9%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,11 +3,12 @@ class Solution {
         int i;
         for(i = 0; i < nums.length; i++){
             if(nums[i] == val){
-                break;
+                break; // fix at first occurence of val
             }
         }
         for(int j = i + 1; j < nums.length; j++){
             if(nums[j] != nums[i]){
+                // if different from target value swap and increment i every time
                 int t = nums[i];
                 nums[i] = nums[j];
                 nums[j] = t;
