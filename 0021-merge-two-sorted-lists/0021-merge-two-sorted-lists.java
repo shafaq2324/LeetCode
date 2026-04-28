22acbc3e3ebed27acf1fbd42fc65405bf76e4318
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-29 02:29:23 +0530

    Time: 0 ms (100%), Space: 44.3 MB (56.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -14,22 +14,28 @@ class Solution {
         ListNode curr = dummy;
 
         while(l1 != null && l2 != null){
+            // iterate both lists till one of them dont end
             if(l1.val <= l2.val){
                 curr.next = l1;
+                // If l1's value is smaller, attach it to the merged list
                 l1 = l1.next;
             }
             else{
                 curr.next = l2;
+                // If l2's value is smaller, attach it to the merged list
                 l2 = l2.next;
             }
+            // move curr's pointer forward
             curr = curr.next;
         }
         if (l1 != null) {
             curr.next = l1;
+            // if l1 remains then connect it directly to curr
         }
 
         if (l2 != null) {
             curr.next = l2;
+            // if l2 remains then connect it directly to curr
         }
         return dummy.next;
     }
