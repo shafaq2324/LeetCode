a01f62add376844b539af647d8b09cb2f4ecd090
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-29 02:43:00 +0530

    Time: 0 ms (100%), Space: 44.9 MB (12.3%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,6 +2,7 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int lb = 0;
         int ub = nums.length - 1;
+        // binary search used to find appropriate insert position if mid not found then return the lb so that the element can be inserted after it
         while(lb <= ub){
             int mid = lb+(ub-lb)/2;
             if(nums[mid] == target){
