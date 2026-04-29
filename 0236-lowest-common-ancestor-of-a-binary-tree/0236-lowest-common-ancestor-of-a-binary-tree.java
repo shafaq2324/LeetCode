5c8d4f486a7114fc8d8f104d934363b630045fe4
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-04-29 21:28:31 +0530

    Time: 7 ms (98.87%), Space: 46.6 MB (23.24%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,14 +9,16 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(root == p || root == q) return root;
-
+        if(root == p || root == q || root == null) return root; 
+        //if we find p or q we signal to parent that the target exists in this branch
+        // if null reached then nothing to find
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-
+        // return only the non null subtree as the null one won't have the target
         if(left == null) return right;
         else if(right == null) return left;
+        // if both are non null then the current root is the LCA
         return root;
     }
 }
+// using dfs as we are finding the deepest common node on paths to 2 given nodes
