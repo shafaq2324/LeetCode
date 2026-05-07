33c865ff4da20fac04c5783c4486b504ad974e58
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-05-07 19:00:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,21 +10,12 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // Start from the root and traverse down
-        while (root != null) {
-            // Case 1: Both nodes are in the right subtree
-            if (p.val > root.val && q.val > root.val) {
-                root = root.right;
-            } 
-            // Case 2: Both nodes are in the left subtree
-            else if (p.val < root.val && q.val < root.val) {
-                root = root.left;
-            } 
-            // Case 3: We found the split point or one of the nodes
-            else {
-                return root;
-            }
-        }
-        return null;
+        if (root == null || root == p || root == q)
+            return root;
+        if (root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+        if (root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+        return root;
     }
 }
