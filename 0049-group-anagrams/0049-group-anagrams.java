50f2cdf3c8d175a36bc4752d29b939d075aa976e
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-06-09 22:11:47 +0530

    Time: 7 ms (73.37%), Space: 49.4 MB (83.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,39 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        boolean[] visited = new boolean[strs.length];
-        
-        for (int i = 0; i < strs.length; i++) {
-            if (visited[i]) {
-                continue; // Skip if this string is already grouped
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str: strs){
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
+            if(!map.containsKey(key)){
+                // create list on first occurence
+                map.put(key, new ArrayList<>());
             }
-            
-            List<String> currentGroup = new ArrayList<>();
-            currentGroup.add(strs[i]);
-            visited[i] = true;
-            
-            for (int j = i + 1; j < strs.length; j++) {
-                if (!visited[j] && isAnagram(strs[i], strs[j])) {
-                    currentGroup.add(strs[j]);
-                    visited[j] = true; // Mark as grouped
-                }
-            }
-            result.add(currentGroup);
+            // add sorted characters : string which created them
+            map.get(key).add(str);
         }
-        
-        return result;
+        return new ArrayList<>(map.values());
     }
 
-    private boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] count = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-        for (int val : count) {
-            if (val != 0) return false;
-        }
-        return true;
-    }
 }
