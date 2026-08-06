a599dd67946a16faf4676dd9d74121c9d6f31658
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-08-06 10:39:34 +0530

    Time: 7 ms (100%), Space: 47.2 MB (25.78%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,16 +1,23 @@
 class ParkingSystem {
 
-    private int[] count;
+    int big, medium, small;
+
     public ParkingSystem(int big, int medium, int small) {
-        count = new int[]{0, big, medium, small};
-        // constructor initializes slots available for every car and null for 0th index
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
     }
     
     public boolean addCar(int carType) {
-        if(count[carType] > 0){
-            count[carType]--;
-            return true;
-            // if slot is available return true and reduce the car slot by 1
+        if(carType == 1){
+            return (big > 0) ? (big-- > 0) : false;
+            // if big car has slot available then return true after comparing and reducing the value otherwise return false
+        }
+        if(carType == 2){
+            return (medium > 0) ? (medium-- > 0) : false;
+        }
+        if(carType == 3){
+            return (small > 0) ? (small-- > 0) : false;
         }
         return false;
     }
