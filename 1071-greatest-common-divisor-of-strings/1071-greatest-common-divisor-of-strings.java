0859dbb41fa745b32f132359733829eed02b0ef2
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-08-10 11:05:42 +0530

    Time: 1 ms (94.74%), Space: 43.4 MB (76.05%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,9 +1,12 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
         if((str1 + str2).equals(str2 + str1)){
+            // if not equal from both sides then no gcd return empty string
             int a = str1.length();
             int b = str2.length();
             while(b != 0){
+                // gcd logic 
+                // if both strings are valid to be computed then just find gcd of their lengths
                 int temp = b;
                 b = a % b;
                 a = temp;
