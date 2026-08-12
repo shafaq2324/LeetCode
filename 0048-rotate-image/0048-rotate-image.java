6c701d105092c40ca99244fe6bd5169c5ff0e7e9
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-08-12 11:01:30 +0530

    Time: 0 ms (100%), Space: 44 MB (16.6%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,18 +1,18 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int b[][] = new int[n][n];
-        int size = n - 1;
-        for(int i=0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                b[j][size-i] = matrix[i][j];
+        int k = n - 1;
+        for(int i=0; i < n >> 1; i++) {
+            for(int j = i; j < k-i; j++) {
+                // the j loop is to swap inner matrix
+                int t = matrix[i][j]; // 0,0
+                matrix[i][j] = matrix[k - j][i]; // 0,0 = 2,0
+                matrix[k-j][i] = matrix[k-i][k-j]; // 2,0 = 2,2
+                matrix[k-i][k-j] = matrix[j][k - i]; // 2,2 = 0,2
+                matrix[j][k - i] = t; // 0,2 = 0,0
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                matrix[i][j] = b[i][j];
-            }
-        }
+        
         
     }
 }
