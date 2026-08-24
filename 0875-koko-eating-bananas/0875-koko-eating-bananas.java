e92171d849bb95fc96815408b3391d96d0d5c0f5
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-08-24 10:50:56 +0530

    Time: 16 ms (34.49%), Space: 48.4 MB (11.22%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int min = 1;
+        int max = Arrays.stream(piles).max().getAsInt();
+        int ans = max;
+        // first assume Koko can eat max number of bananas 
+        while(min <= max){
+            int mid = min + (max - min) / 2;
+            long hours = 0;
+            for(int pile: piles){
+                long div = pile / mid;
+                hours += div;
+                if(pile % mid != 0) hours++;
+            }
+            if(hours <= h){
+                ans = mid;
+                max = mid - 1;
+            }
+            else{
+                min = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
