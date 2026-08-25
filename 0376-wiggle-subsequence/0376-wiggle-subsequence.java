4bc444fa12035acb56b90ccbb494cd7225b7aad3
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-08-25 10:50:56 +0530

    Time: 0 ms (100%), Space: 42.9 MB (55.51%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.java
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -5,10 +5,10 @@ class Solution {
         int down = 1;
         for(int i = 1; i < nums.length; i++){
             if(nums[i] > nums[i-1]){
-                up = down + 1;
+                up = down + 1; // positive result
             }
             else if (nums[i] < nums[i-1]){
-                down = up + 1;
+                down = up + 1; // negative result
             }
         }
         return Math.max(up, down);
