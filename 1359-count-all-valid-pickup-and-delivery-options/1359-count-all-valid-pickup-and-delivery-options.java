acfa537f75977351e37447640cb93e6737399f4f
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2026-09-03 10:26:15 +0530

    Time: 0 ms (100%), Space: 42 MB (58.82%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,31 +1,8 @@
 class Solution {
-    long MOD = 1_000_000_000 + 7L;
-    long dp[][];
     public int countOrders(int n) {
-        dp = new long[n+1][n+1];
-
-        for(int i=0;i<=n;i++)
-            Arrays.fill(dp[i],-1);
-
-        long res = helper(n,n);
-        return (int)(res % MOD);
-    }
-
-    private long helper(int p,int d){
-        if(p == 0 && d == 0)
-            return 1L;
-        
-        if(dp[p][d] != -1)
-            return dp[p][d];
-
-        long takeP = 0L , takeD = 0L;
-
-        if(p != 0)
-            takeP = (p * helper(p-1,d))%MOD;
-        
-        if(d != 0 && d > p)
-            takeD = ((d-p) * helper(p,d-1))%MOD;
-
-        return dp[p][d] = (takeP + takeD)%MOD;
+        long res = 1, mod = (long)1e9 + 7;
+        for (int i = 1; i <= n; ++i)
+            res = res * (i * 2 - 1) * i % mod;
+        return (int)res;
     }
 }
